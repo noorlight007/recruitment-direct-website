9b77ed2160f601ada5a0ae2b5d5a89b364162bfc
Md. Nazim Ahmed <77225770+nazimnk9@users.noreply.github.com>
2026-08-18 19:07:30 +0600

    solve

--- next.config.js
+++ next.config.js
@@ -44,7 +44,7 @@ const nextConfig = {
                     },
                     {
                         key: 'Content-Security-Policy',
-                        value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://app.trysoro.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' data: blob: *; font-src 'self' https://fonts.gstatic.com data:; connect-src 'self' *; frame-src 'self' https://www.googletagmanager.com; frame-ancestors 'self'; worker-src 'self' blob:; child-src 'self' blob:;",
+                        value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://app.trysoro.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' data: blob: *; font-src 'self' https://fonts.gstatic.com data:; connect-src 'self' *; frame-src 'self' https://www.googletagmanager.com https://www.google.com https://maps.google.com; frame-ancestors 'self'; worker-src 'self' blob:; child-src 'self' blob:;",
                     },
                 ],
             },
